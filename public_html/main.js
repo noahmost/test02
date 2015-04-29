64c4452401abceb9b34a02267d3770d630a0780f
Noah Mostacero <noahmostacero@gmail.com>
2015-04-29 10:42:05 -0700

    Commit

---
+++ public_html/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+
+	alert("u clicked un me");
+
+	});
+
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+
+	});
+
+});
